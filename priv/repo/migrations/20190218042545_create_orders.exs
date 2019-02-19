5d477dc9b67f81acfe87fdf193eb3197102089b5
Peter Mueller <peter.mueller@beam.dental>
2019-02-18 22:04:22 -0500

    require associations for orders at DB level

--- priv/repo/migrations/20190218042545_create_orders.exs
+++ priv/repo/migrations/20190218042545_create_orders.exs
@@ -3,9 +3,9 @@ defmodule RxDelivery.Repo.Migrations.CreateOrders do
 
   def change do
     create table(:orders) do
-      add :patient_id, references(:patients, on_delete: :nothing)
-      add :prescription_id, references(:prescriptions, on_delete: :nothing)
-      add :location_id, references(:locations, on_delete: :nothing)
+      add :patient_id, references(:patients, on_delete: :restrict), null: false
+      add :prescription_id, references(:prescriptions, on_delete: :restrict), null: false
+      add :location_id, references(:locations, on_delete: :restrict), null: false
 
       timestamps()
     end
